17938af3c40f193c268ce24e3538c3e06b8e40ae
MarcoAGreco <marcoandreagreco@gmail.com>
2022-01-04 16:44:51 +0100

    [PM-390] add flag for signing procedure

--- UpdateBuyerBanks/handler.ts
+++ UpdateBuyerBanks/handler.ts
@@ -1,18 +1,17 @@
 /* eslint-disable no-console */
 import { Context } from "@azure/functions";
-import { pipe } from "fp-ts/lib/function";
+import { pipe, flow } from "fp-ts/lib/function";
 import * as TE from "fp-ts/lib/TaskEither";
 import * as E from "fp-ts/Either";
 import { BlobServiceClient } from "@azure/storage-blob";
 import * as MyBankClient from "../generated/definitions/mybank/client";
-import { getPayerPSPsSCT01Request } from "../generated/definitions/mybank/getPayerPSPsSCT01Request";
 import { withApiRequestWrapper } from "../utils/api";
 import { getConfigOrThrow } from "../utils/config";
 import { fetchApi } from "../utils/fetch";
 import { getLogger } from "../utils/logging";
 import { sign } from "../utils/auth";
 import { setDayBlobTask } from "../services/storage";
-// import { toDefaultResponseErrorInternal } from "../utils/responses";
+import { getPayerPSPsSCT01Request } from "../generated/definitions/mybank/getPayerPSPsSCT01Request";
 
 /*
  * Time triggered function for daily buyerbank list update
@@ -27,8 +26,8 @@ const mybankclient = MyBankClient.createClient({
 
 const body: getPayerPSPsSCT01Request = {
   input: {
-    branch: "branch",
-    institute: "institute"
+    branch: conf.MY_BANK_BRANCH,
+    institute: conf.MY_BANK_INSTITUTE
   }
 };
 
@@ -48,49 +47,62 @@ export const updateBuyerBank = async (
   );
   const logger = getLogger(context, "BuyerBankService", "UpdateBuyerBank");
 
-  pipe(
-    sign(
-      JSON.stringify(body),
-      conf.MY_BANK_KEY.toString(),
-      conf.MY_BANK_CERT_PASSPHRASE.toString(),
-      conf.MY_BANK_SIGN_ALG.toString()
+  const signedBody = flow(
+    E.fromPredicate(
+      signature => signature as boolean,
+      _ => _
     ),
-    E.fold(
-      (e: Error) => logger.logUnknown(e),
-      async (signature: string) =>
-        await pipe(
-          withApiRequestWrapper(
-            getLogger(context, "BuyerBankService", "UpdateBuyerBank"),
-            () =>
-              mybankclient.getPayerPSPsSCT01({
-                ...params,
-                ...{ "X-Signature": signature }
-              }),
-            200
-          ),
-          TE.mapLeft(err => logger.logUnknown(err)),
-          TE.map(async res => {
-            const blobClient = BlobServiceClient.fromConnectionString(
-              conf.MY_BANK_AZ_STORAGE_CONN_STRING
-            );
-            await pipe(
-              setDayBlobTask(
-                blobClient,
-                conf.MY_BANK_CONTAINER_NAME,
-                JSON.stringify(res)
-              ),
-              TE.mapLeft(err => logger.logUnknown(err)),
-              TE.map(resp =>
-                logger.logInfo(
-                  `Response: ${JSON.stringify(
-                    // eslint-disable-next-line no-underscore-dangle
-                    resp._response.status.toString()
-                  )}`
-                )
-              )
-            )();
-          })
-        )()
-    )
-  );
+    E.map(signature => signature),
+    E.mapLeft(_ =>
+      pipe(
+        sign(
+          JSON.stringify(body),
+          conf.MY_BANK_KEY.toString(),
+          conf.MY_BANK_CERT_PASSPHRASE.toString(),
+          conf.MY_BANK_SIGN_ALG.toString()
+        ),
+        E.fold(
+          (err: Error) => logger.logUnknown(err),
+          (signature: string) => signature
+        )
+      )
+    ),
+    E.toUnion
+  )(conf.MY_BANK_SIGNATURE);
+
+  await pipe(
+    withApiRequestWrapper(
+      getLogger(context, "BuyerBankService", "UpdateBuyerBank"),
+      () =>
+        mybankclient.getPayerPSPsSCT01({
+          ...params,
+          ...{ "X-Signature": signedBody as string }
+        }),
+      200
+    ),
+    TE.mapLeft(err => logger.logUnknown(err)),
+    TE.map(async res => {
+      const blobClient = BlobServiceClient.fromConnectionString(
+        conf.MY_BANK_AZ_STORAGE_CONN_STRING
+      );
+      await pipe(
+        setDayBlobTask(
+          blobClient,
+          conf.MY_BANK_CONTAINER_NAME,
+          JSON.stringify(res)
+        ),
+        TE.mapLeft(err => {
+          logger.logUnknown(err);
+        }),
+        TE.map(resp =>
+          logger.logInfo(
+            `Response: ${JSON.stringify(
+              // eslint-disable-next-line no-underscore-dangle
+              resp._response.status.toString()
+            )} `
+          )
+        )
+      )();
+    })
+  )();
 };
