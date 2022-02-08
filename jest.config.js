c55124600d3d1df9e9f6f6b68e9bac171f3fc7ab
MarcoAGreco <marcoandreagreco@gmail.com>
2022-02-08 10:51:15 +0100

    fix: update test reporters

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["dist", "/node_modules"]
+  testPathIgnorePatterns: ["dist", "/node_modules"],
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: './test_reports',
+      outputName: 'pagopa-functions-checkout-TEST.xml',
+    }]
+  ],
+  coverageReporters: ["cobertura"],
+  testMatch: [
+    "**/__tests__/*.ts"
+  ]
 };
